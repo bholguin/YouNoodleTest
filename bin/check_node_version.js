7cc52b3c69a8234589e40d5bed0cc337cc525b65
Brayhan Holguin <bholguin1488@gmail.com>
2024-07-12 12:06:08 -0500

    validate node version to run the project

--- bin/check_node_version.js
+++ bin/check_node_version.js
@@ -1,10 +1,11 @@
-const semver = require('semver');
-const {engines} = require('../package');
+const semver = require('semver')
 
-const version = engines.node;
+const { engines } = require('../package')
+
+const version = engines.node
 
 if (!semver.satisfies(process.version, version)) {
-  throw new Error(
-    `The current node version${process.version} does not satisfy the required version ${version}.`
-  );
+    throw new Error(
+        `The current node version${process.version} does not satisfy the required version ${version}.`,
+    )
 }
